9ac612cf8e5b6f460a5598324295911719dd0c96
Venkatesh <104349833+Venkatesh5652@users.noreply.github.com>
2022-10-09 21:44:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Welocme to Dialy Journal "
+const aboutContent = "This is a Blog site where every person can write down their thoughts and feelings to understand them more clearly";
+const contactContent = {
+                  name : "©️ Douluri Venkatesh",
+                  email: "doulurivenkatesh@gmail.com",
+                  phone: "7989524134"
+                };
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/",function(req,res)
+{
+  res.render("home",{
+    StartingContent: homeStartingContent ,
+    posts: posts
+    });
+});
+
+
+app.get("/about",function(req,res)
+{
+  res.render("about",{aboutContent: aboutContent });
+});
+
+app.get("/contact",function(req,res)
+{
+  res.render("contact",{contactContent: contactContent });
+});
+
+app.get("/compose",function(req,res)
+{
+  var today= new Date();
+  var options = { weekday: "long", month: "long", day: "numeric" };
+  var day = today.toLocaleDateString("en-US", options);
+     res.render("compose",{ todaydate:day});
+   });
+
+app.post("/compose",function(req,res)
+{
+    const post={
+      title:req.body.postTitle,
+      content:req.body.postBody,
+    };
+      posts.push(post);
+
+      res.redirect("/");
+});
+
+app.get("/posts/:postName",function(req,res)
+{
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+    posts.forEach(function(post)
+    {
+    const storedTitle = _.lowerCase(post.title);
+
+    if(storedTitle === requestedTitle)
+    {
+        res.render("post",{
+          title: post.title,
+          content: post.content,
+        });
+    }
+  });
+
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
