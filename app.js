cf993aef33e8539420ccb6fa3b0fcd5ff9e5dbf3
Venkatesh <104349833+Venkatesh5652@users.noreply.github.com>
2022-10-09 22:23:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,6 +81,6 @@ app.get("/posts/:postName",function(req,res)
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT|| 3000, () =>{
+    console.log("Express server listening on port" );
+  });
